bde74016a9243ac395c5ef2168eea3bcb87ad5c6
Abdulrhman Mohamed <138893669+AYeagerr@users.noreply.github.com>
2025-03-10 22:22:21 +0200

    Delete js/skills-bar.js

--- js/skills-bar.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-    document.querySelector('.skill-html').style.width = '100%';
-    document.querySelector('.skill-css').style.width = '75%';
-    document.querySelector('.skill-js').style.width = '50%';
-});
